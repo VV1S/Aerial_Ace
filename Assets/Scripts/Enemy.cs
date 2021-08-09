e58e98dc804a8415f38be8946cf6b6c8d1465de9
Robert <robertjwisniowski@gmail.com>
2021-08-09 18:16:45 +0200

    Enemy hit points system has been added.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] GameObject death;
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] GameObject hitVFX;
     [SerializeField] Transform parent;
     [SerializeField] int scoreToIncrease=20;
+    [SerializeField] int hitPoints = 2;
+    [SerializeField] int dealedDamage = 1;
 
     ScoreBoard scoreBoard;
 
@@ -18,17 +21,23 @@ public class Enemy : MonoBehaviour
     private void OnParticleCollision(GameObject other)
     {
         ProcessHit();
-        KillEnemy();
+        if (hitPoints < 1)
+        {
+            KillEnemy();
+        }
     }
 
     private void ProcessHit()
     {
+        GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
+        hitPoints -= dealedDamage;
         scoreBoard.IncreaseScore(scoreToIncrease);
     }
 
     private void KillEnemy()
     {
-        GameObject vfx = Instantiate(death, transform.position, Quaternion.identity);
+        GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
         vfx.transform.parent = parent;
         Destroy(gameObject);
     }
